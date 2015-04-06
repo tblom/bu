1aca8cad80243ff62f7ea2f5266c7902a811e788
Thomas Blom <thomas@bpro.attlocal.net>
2015-04-05 22:38:55 -0500

    mods to bu sketch design and initial xcode project

---
+++ ios/blueunicorn/blueunicorn/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  blueunicorn
+//
+//  Created by Thomas Blom on 4/4/15.
+//  Copyright (c) 2015 Good Karma Software. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
